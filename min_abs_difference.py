c2e7c452963be4ba7d2e894ed731e4191f17e38a
amithachari <64373075+amithachari@users.noreply.github.com>
2022-01-14 03:59:17 -0600

    Create min_abs_difference.py

---
+++ min_abs_difference.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    # Write your code here
+    diff = abs(arr[0]- arr[1])
+    arr.sort()
+    for i in range(len(arr)-1):
+        if diff > abs(arr[i]- arr[i+1]):
+            diff = abs(arr[i]- arr[i+1])
+    return diff
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
