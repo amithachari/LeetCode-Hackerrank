78822f44837a40abdbc9495dff0172392de2b726
amithachari <64373075+amithachari@users.noreply.github.com>
2022-02-09 23:20:10 -0600

    Create sherlockandArray.py

---
+++ sherlockandArray.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    LHS = 0
+    RHS = sum(arr)
+    print()
+    for i in range(len(arr)):
+        if LHS == RHS - LHS - arr[i]:
+            return "YES"
+        LHS+=arr[i]
+
+
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
