096035c901ef1d7dea5a448825ae7594eb56448c
amithachari <64373075+amithachari@users.noreply.github.com>
2022-02-09 23:37:13 -0600

    Create misereNim.py

---
+++ misereNim.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'misereNim' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY s as parameter.
+#
+from functools import reduce
+
+def misereNim(s):
+    if(max(s)==1):
+        return "First" if (len(s) % 2 ==0) else "Second"
+    else:
+        xor = reduce((lambda x, y: x ^ y), s)
+        return "Second" if xor == 0 else "First"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        s = list(map(int, input().rstrip().split()))
+
+        result = misereNim(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
