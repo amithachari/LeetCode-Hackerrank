75915b5bb9300e3d7e094d0b6ebc842c7a2eb326
amithachari <64373075+amithachari@users.noreply.github.com>
2022-02-10 00:21:22 -0600

    Create gaming_array1.py

---
+++ gaming_array1.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gamingArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def gamingArray(arr):
+    count_max = 0
+    last_max = 0
+    for a in arr:
+        if a > last_max: 
+            last_max = a
+            count_max +=1
+    return "ANDY" if count_max % 2 == 0 else "BOB"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input().strip())
+
+    for g_itr in range(g):
+        arr_count = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = gamingArray(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
